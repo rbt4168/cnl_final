c85e2d848a45ac15632f937a35a60e8039e2c75b
rbt4168 <micyeh92mic@gmail.com>
2024-06-04 19:57:22 +0800

    feat: modified server

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import socket
-
+import random
 # Set up the UDP client
 UDP_IP = "0.0.0.0"  # Listen on all available interfaces
 UDP_PORT = 5005     # The port to listen on
@@ -13,10 +13,11 @@ print(f"Listening on {UDP_IP}:{UDP_PORT}")
 while True:
     # Receive data from the socket
     data, addr = sock.recvfrom(1024)  # Buffer size is 1024 bytes
-    
-    print(f"Received message: {data.decode()} from {addr}")
-    
+    rand = random.getrandbits(32)
+    print(f"Received message: {data.decode()} from {addr}, number {rand}")
+
     # Send a response back to the sender
-    response_message = "Hello, UDP server!"
+    response_message = f"Hello, UDP server! Number {rand}"
+    print("My response message is:", response_message)
     sock.sendto(response_message.encode(), addr)
     print(f"Sent response to {addr}")
